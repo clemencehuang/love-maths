730bd5110d29908ab03cdfb714b6de202bf85e0b
Clémence HUANG <153006617+clemencehuang@users.noreply.github.com>
2024-05-07 12:41:13 +0100

    add multiply, substract code, flavicon and tidied up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-// for (let i = 0; i < buttons.length; i++){} -> this is an older syntax form of writing this code, 
-// the following is more modern as we don't use index notation to speak to elements in the array.
-
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -18,38 +15,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 
 });
 
-/** 
- * This is a docstring. Docstrings are great for understanding the functionality 
- * of the larger part of the code, i.e., the general purpose of any class, module, 
- * or function, whereas the comments are used for code, statements, and expressions, 
- * which tend to be small.
+/**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-
 function runGame(gameType) {
 
-/** 
- * The Math.floor() static method always rounds down and returns the largest integer 
-* less than or equal to a given number.
-* console.log(Math.floor(5.95));
-// Expected output: 5
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
-console.log(Math.floor(5.05));
-// Expected output: 5
-
-* The following creates two random numbers between 1 and 25
-*/
-    
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract" ) {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -58,21 +52,24 @@ console.log(Math.floor(5.05));
 }
 
 /**
- * Gets the operands (the numbers) and the operator (plus, minus etc)
- * directly from the dom, and returns the correct answer.
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
  */
-function calculateCorrectAnswer() {
+function checkAnswer() {
 
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
@@ -88,6 +85,10 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -123,10 +124,18 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 }
